4392f43d0d044b3b8b213e5dd434380d10265300
Hideki Tanaka <work@tuxsnct.com>
2021-12-05 17:12:44 +0900

    add the minimum required source code

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,10 +1,22 @@
-import { h, FunctionalComponent } from 'preact'
+import { Router } from '@reach/router'
+import React from 'react'
+import { GlobalStyles } from 'twin.macro'
+// eslint-disable-next-line import/no-unresolved
+import { useRegisterSW } from 'virtual:pwa-register/react'
+import { Home } from '../routes'
 
-const App: FunctionalComponent = () => (
-  <div>
-    <h1>Hello World!</h1>
-  </div>
-)
+const App: React.FC = () => {
+  useRegisterSW()
+
+  return (
+    <>
+      <GlobalStyles />
+      <Router>
+        <Home path="/" />
+      </Router>
+    </>
+  )
+}
 
 export {
   App
---
+++ src/routes/home.tsx
@@ -0,0 +1,12 @@
+import { RouteComponentProps } from '@reach/router'
+import React from 'react'
+
+const Home: React.FC<RouteComponentProps> = () => (
+  <div>
+    <h1>Hello World!</h1>
+  </div>
+)
+
+export {
+  Home
+}
