e1da716d1fd35f9e6aa46815ce2e39441c5053e9
Hideki Tanaka <work@tuxsnct.com>
2021-12-05 17:09:48 +0900

    set up configurations files

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import preactPlugin from '@preact/preset-vite'
 import { UserConfig } from 'vite'
+import babelMacrosPlugin from 'vite-plugin-babel-macros'
+import { VitePWA as vitePwaPlugin } from 'vite-plugin-pwa'
 
 const config: UserConfig = {
   base: './',
-  plugins: [preactPlugin()]
+  plugins: [
+    babelMacrosPlugin(),
+    preactPlugin(),
+    vitePwaPlugin({
+      registerType: 'autoUpdate',
+      workbox: {
+        inlineWorkboxRuntime: true
+      }
+    })
+  ]
 }
 
 export default config
