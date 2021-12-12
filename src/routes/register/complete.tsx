b58b8c2e4c3cb03b3b7179f05db365e049098d84
Hideki Tanaka <work@tuxsnct.com>
2021-12-12 13:31:07 +0900

    add a todo to registration completion page

--- src/routes/register/complete.tsx
+++ src/routes/register/complete.tsx
@@ -12,6 +12,7 @@ const RegisterComplete: React.FC = () => {
     <CenteringLayout title={['落とし物の登録が完了しました!', '学生課の人に、落とし物を渡してください。']}>
       <div tw="flex flex-col items-center mx-24">
         <div tw="my-12 space-x-32 flex justify-between items-center">
+          {/* TODO: valueを正しい値に置き換える */}
           <QRCode value="https://github.com/hacku-kosen-snct/webapp" size={240} css={tw`bg-white p-6 rounded-xl`} />
           <Text tw="text-lg">
           LINE BotからQRコードリーダーを開いて、このQRコードを読むことで、落とし物の持ち主からメッセージが届くかもしれません。
