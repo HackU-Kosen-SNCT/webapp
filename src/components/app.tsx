2fe570644890352ce4177b572adfa48870e148dc
Hideki Tanaka <work@tuxsnct.com>
2021-12-12 16:53:55 +0900

    rename from routes to pages

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 import { GlobalStyles } from 'twin.macro'
 // eslint-disable-next-line import/no-unresolved
 import { useRegisterSW } from 'virtual:pwa-register/react'
-import { Switch, Route, Router, Redirect, useLocation, useRouter } from 'wouter'
-import * as routes from '../routes'
+import { Switch, Route, Redirect } from 'wouter'
+import * as pages from '../pages'
 
 const customStyles = css`
   * {
@@ -23,24 +23,24 @@ const App: React.FC = () => {
       <GlobalStyles />
       <Global styles={customStyles} />
       <Switch>
-        <Route path="/" component={routes.Home} />
-        <Route path="/lafs" component={routes.Lafs} />
+        <Route path="/" component={pages.Home} />
+        <Route path="/lafs" component={pages.Lafs} />
         <Route path="/receive">
           <Redirect to="/receive/select-laf" replace={true} />
         </Route>
-        <Route path="/receive/select-laf" component={routes.ReceiveSelectLaf} />
-        <Route path="/receive/confirm" component={routes.ReceiveConfirm} />
-        <Route path="/receive/send-letter" component={routes.ReceiveSendLetter} />
-        <Route path="/receive/complete" component={routes.ReceiveComplete} />
+        <Route path="/receive/select-laf" component={pages.ReceiveSelectLaf} />
+        <Route path="/receive/confirm" component={pages.ReceiveConfirm} />
+        <Route path="/receive/send-letter" component={pages.ReceiveSendLetter} />
+        <Route path="/receive/complete" component={pages.ReceiveComplete} />
         <Route path="/register">
           <Redirect to="/register/select-category" replace={true} />
         </Route>
-        <Route path="/register/select-category" component={routes.RegisterSelectCategory} />
-        <Route path="/register/overview" component={routes.RegisterOverview} />
-        <Route path="/register/photograph" component={routes.RegisterPhotograph} />
-        <Route path="/register/confirm" component={routes.RegisterConfirm} />
-        <Route path="/register/complete" component={routes.RegisterComplete} />
-        <Route component={routes.NotFound} />
+        <Route path="/register/select-category" component={pages.RegisterSelectCategory} />
+        <Route path="/register/overview" component={pages.RegisterOverview} />
+        <Route path="/register/photograph" component={pages.RegisterPhotograph} />
+        <Route path="/register/confirm" component={pages.RegisterConfirm} />
+        <Route path="/register/complete" component={pages.RegisterComplete} />
+        <Route component={pages.NotFound} />
       </Switch>
     </>
   )
