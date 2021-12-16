a03d8b6605bfd5c1500f4ca3019ab9a6901cd474
Bayathy <misosoup0321@icloud.com>
2021-12-17 02:51:16 +0900

    サンプル画像の差し替え

--- src/pages/lafs.tsx
+++ src/pages/lafs.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'preact/hooks'
 import React, { useEffect } from 'react'
+import { RiCustomerServiceLine } from 'react-icons/ri'
+import { VscJersey } from 'react-icons/vsc'
 import tw from 'twin.macro'
 import { BackButton, LafShelf, Modal } from '../components'
 import { FixedLayout } from '../layouts'
@@ -7,7 +9,9 @@ import { FixedLayout } from '../layouts'
 // TODO: このページを完成させる
 const Lafs: React.FC = () => {
   const [Modalstate, setModalstate] = useState<boolean>(false)
-  const [Modalimg, setModalimg] = useState<string>()
+  const [Modalimg, setModalimg] = useState<string>('')
+
+  const testimg = 'https://zukan.com/media/comment/original/comment_6641_20181206112123.jpg'
 
   useEffect(() => {
     // 表示されたときに、バックエンドからデータを取得してstateにぶちこむ
@@ -29,16 +33,18 @@ const Lafs: React.FC = () => {
           <div tw="w-1/2 m-0 m-auto"><img tw="rounded-xl" alt="落とし物の画像" src={Modalimg}/></div>
           <div tw="flex">
           </div>
+          <VscJersey/>
+          <RiCustomerServiceLine/>
         </div>
       </Modal>
 
       <LafShelf
         name="教科書・のーと・ファイル"
         color="#FF00FF"
-        imgurl="http://free-photo.net/photo_img/081241411241.jpg"
+        imgurl = {testimg}
         onClick={() => {
           setModalstate(true)
-          setModalimg('http://free-photo.net/photo_img/081241411241.jpg')
+          setModalimg(testimg)
         }}/>
     </FixedLayout>
   )
