1c3f9be0344d07aba7209d80bf114d6c16c43deb
Re-taro <aminokacho@gmail.com>
2021-12-16 22:33:44 +0900

    ✨ Fetch in useEffect

--- src/pages/receive/select-laf.tsx
+++ src/pages/receive/select-laf.tsx
@@ -1,11 +1,41 @@
-import React, { useEffect } from 'react'
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable no-console */
+import axios, { AxiosPromise } from 'axios'
+import React, { useEffect, useState } from 'react'
 import { BackButton, Modal, Progress } from '../../components'
 import { FixedLayout } from '../../layouts'
 
-// TODO: このページを完成させる
+type item = {
+  item_id: string
+  category: string
+  color: string
+  detail: string | null
+  image_url: string
+  created_at: Date
+  received_at: Date | null
+}
+
+const client = axios.create({
+  baseURL: 'https://togather-api.takumma.net/',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  responseType: 'json'
+})
+
+const fetchAllItems = ():AxiosPromise<item> => client.get('laf')
+
 const ReceiveSelectLaf: React.FC = () => {
+  const [lafs, setLafs] = useState<item>()
+  const [error, setError] = useState<boolean>(false)
   useEffect(() => {
-    // 表示されたときに、バックエンドからデータを取得してstateにぶちこむ
+    fetchAllItems()
+      .then((response) => {
+        setLafs(response.data)
+      })
+      .catch(() => {
+        setError(true)
+      })
   }, [])
 
   return (
