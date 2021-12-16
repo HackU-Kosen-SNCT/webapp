23e1cafb0588c76e44f42fa48ca231d21a631ebd
Bayathy <misosoup0321@icloud.com>
2021-12-17 04:18:41 +0900

    モーダル画面をつくった。俺はいま無敵だ

--- src/pages/lafs.tsx
+++ src/pages/lafs.tsx
@@ -1,15 +1,17 @@
 import { useState } from 'preact/hooks'
 import React, { useEffect } from 'react'
-import { RiCustomerServiceLine } from 'react-icons/ri'
 import { VscJersey } from 'react-icons/vsc'
 import tw from 'twin.macro'
 import { BackButton, LafShelf, Modal } from '../components'
 import { FixedLayout } from '../layouts'
 
 // TODO: このページを完成させる
+// eslint-disable-next-line max-lines-per-function
 const Lafs: React.FC = () => {
   const [Modalstate, setModalstate] = useState<boolean>(false)
   const [Modalimg, setModalimg] = useState<string>('')
+  const [ModalDetail, setModaldetail] = useState<string>('')
+  const [, setModalcolor] = useState<string>('')
 
   const testimg = 'https://zukan.com/media/comment/original/comment_6641_20181206112123.jpg'
 
@@ -29,12 +31,20 @@ const Lafs: React.FC = () => {
       <Modal active = {Modalstate}>
         <div tw="w-1/2 h-3/4 bg-lightgrey2 rounded-xl">
           <div tw="w-full mt-14 mb-6 text-center text-primarydeep
-           text-3xl font-semibold underline">この落とし物を受け取りますか?</div>
+            text-3xl font-semibold underline">この落とし物を受け取りますか?</div>
           <div tw="w-1/2 m-0 m-auto"><img tw="rounded-xl" alt="落とし物の画像" src={Modalimg}/></div>
-          <div tw="flex">
+          <div tw="flex justify-center">
+            <div tw="bg-white w-20 rounded-xl mt-3 text-center font-semibold shadow-custom">
+              <VscJersey tw="m-0 m-auto" size={70}/>衣料品
+            </div>
           </div>
-          <VscJersey/>
-          <RiCustomerServiceLine/>
+          <div tw="w-full text-center mt-10">{ModalDetail}</div>
+          <div tw="flex justify-center ">
+            <button onClick={() => setModalstate(false)}
+              tw="bg-white w-1/4 mt-10 shadow-custom pl-5 pr-5 pt-4 pb-4 rounded-3xl">
+                受け取る
+            </button>
+          </div>
         </div>
       </Modal>
 
@@ -45,6 +55,8 @@ const Lafs: React.FC = () => {
         onClick={() => {
           setModalstate(true)
           setModalimg(testimg)
+          setModalcolor('#FF00FF')
+          setModaldetail('夢と希望はtogatherで拾えますか')
         }}/>
     </FixedLayout>
   )
