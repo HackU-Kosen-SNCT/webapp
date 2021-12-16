9fea78736ada0021f6713ccee78f9457b7c7f0d5
Re-taro <aminokacho@gmail.com>
2021-12-17 00:59:17 +0900

    🚧 I made a template
    ToDo: Use Shelf components and make a list of lafs

--- src/pages/receive/select-laf.tsx
+++ src/pages/receive/select-laf.tsx
@@ -1,7 +1,8 @@
-/* eslint-disable eslint-comments/disable-enable-pair */
-/* eslint-disable no-console */
 import axios, { AxiosPromise } from 'axios'
 import React, { useEffect, useState } from 'react'
+import tw from 'twin.macro'
+import undrawDogWorking from '../../assets/undraw_dog_walking_re_l61p 1.svg'
+import undrawEmpty from '../../assets/undraw_empty.svg'
 import { BackButton, Modal, Progress } from '../../components'
 import { FixedLayout } from '../../layouts'
 
@@ -23,10 +24,10 @@ const client = axios.create({
   responseType: 'json'
 })
 
-const fetchAllItems = ():AxiosPromise<item> => client.get('laf')
+const fetchAllItems = ():AxiosPromise<item[]> => client.get('laf')
 
 const ReceiveSelectLaf: React.FC = () => {
-  const [lafs, setLafs] = useState<item>()
+  const [lafs, setLafs] = useState<item[]>([])
   const [error, setError] = useState<boolean>(false)
   useEffect(() => {
     fetchAllItems()
@@ -37,7 +38,6 @@ const ReceiveSelectLaf: React.FC = () => {
         setError(true)
       })
   }, [])
-
   return (
     <FixedLayout
       title="受け取る落とし物を選択してください"
@@ -48,7 +48,23 @@ const ReceiveSelectLaf: React.FC = () => {
         </>
       }
     >
-      <Modal />
+      { error
+        ? (
+          <div tw="w-full flex flex-col items-center space-y-8">
+            <img src={undrawDogWorking as string} alt="犬と散歩♡" tw="h-80 rounded-3xl" />
+            <span tw="text-2xl">エラーが発生しました。犬の散歩でもしましょう。</span>
+          </div>
+        )
+        : (lafs.length === 0
+          ? (
+            <div tw="w-full flex flex-col items-center space-y-8">
+              <img src={undrawEmpty as string} alt="からっぽ" tw="h-80 rounded-3xl" />
+              <span tw="text-2xl">ええと、落とし物は何も届いていません。</span>
+            </div>
+          )
+          : (
+            <Modal />
+          ))}
     </FixedLayout>
   )
 }
