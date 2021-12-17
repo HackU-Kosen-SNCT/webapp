aa0c83d6bc887543b7e07ff10a04008e3fbeb282
Hideki Tanaka <work@tuxsnct.com>
2021-12-18 02:14:49 +0900

    fix an error on the receive confirmation page

--- src/components/overview.tsx
+++ src/components/overview.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import tw, { css } from 'twin.macro'
 import { categories, categoryTexts, toAllowCategory } from '../category'
-import { CategoryButton, Color, Text, TextButton } from './'
+import { CategoryButton, Color, Modalicon, Text, TextButton } from './'
 
 type LafOverviewProperties = {
   imageSource: React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>['src'],
@@ -24,13 +24,10 @@ const LafOverview: React.FC<LafOverviewProperties> = ({
   <div tw="flex flex-col items-center space-y-8" {...rest}>
     <img src={imageSource} alt={category} tw="h-60 rounded-3xl" />
     <div tw="w-full flex  items-center justify-evenly">
-      <CategoryButton
+      <Modalicon
         tw="pointer-events-none"
-        icon={categories[toAllowCategory(category)].icon}
-        iconStyles={css(tw`w-12 h-12`)}
-      >
-        {category}
-      </CategoryButton>
+        category={category}
+      />
       <Color color={color} tw="pointer-events-none" />
     </div>
     <p tw="max-w-xl overflow-hidden truncate text-xl font-semibold">
